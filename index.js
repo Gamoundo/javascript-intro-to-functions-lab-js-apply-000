507df565d2d8d4fe5fba23202b402fdceef0f307
Akil Griffiths <akilgg@gmail.com>
2020-02-22 01:14:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function whisper(string) {
+  return (string).toLowerCase();
+}
+function logShout(string) {
+  return console.log(string).toUpperCase();
+}
+function shout(string){
+  return (string).toUpperCase();
+}
+function logWhisper(string) {
+  return console.log(string).toLowercase();
+}
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma.") {
+    return ("I love you, too")
+  }
+  
+}
